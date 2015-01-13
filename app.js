1d772eb62bd4bb3bc351761e41369acd0fb64c3f
Nejc Štebe <nejc.stebe92@gmail.com>
2015-01-13 20:20:38 +0100

    jade set up, log in page started

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'jade');
 
+app.use('/static/css', express.static(__dirname + '/assets/css/'));
 
 app.get('/', function(req, res) {
-	res.render('index', { title: 'Hey', message: 'Hello there!'});
+	res.render('index', { title: 'Hey'});
 });
 
 app.listen(process.env.PORT || 1337);
